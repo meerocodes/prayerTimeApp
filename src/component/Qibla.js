d7e7e0bd1513b00bd5816314e86317ed0abdef93
Amir Abdel Rahman <meero@Amirs-MacBook-Pro.local>
2023-03-26 18:05:03 -0400

    created a qibla component using the geolocation browser api to create a real time compass

--- src/component/Qibla.js
+++ src/component/Qibla.js
@@ -1,36 +1,28 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
+import { Qibla } from 'qibla';
 
-function QiblaCompass() {
-  const [qiblaDirection, setQiblaDirection] = useState(null);
+const QiblaCompass = () => {
+  const [direction, setDirection] = useState(0);
 
   useEffect(() => {
-    // Use the Geolocation API to get the user's current location
-    navigator.geolocation.getCurrentPosition((position) => {
-      const latitude = position.coords.latitude;
-      const longitude = position.coords.longitude;
-
-      // Calculate the qibla direction
-      const phiK = 21.4 * Math.PI / 180.0;
-      const lambdaK = 39.8 * Math.PI / 180.0;
-      const phi = latitude * Math.PI / 180.0;
-      const lambda = longitude * Math.PI / 180.0;
-      const psi = Math.atan2(Math.sin(lambdaK - lambda), Math.cos(phi) * Math.tan(phiK) - Math.sin(phi) * Math.cos(lambdaK - lambda));
-      const qiblaDirection = (psi * 180.0 / Math.PI + 360) % 360;
-      setQiblaDirection(qiblaDirection);
-    });
+    const handleOrientationChange = event => {
+      const kaabaDirection = Qibla(event.alpha, event.beta, event.gamma);
+      setDirection(kaabaDirection);
+    };
+    window.addEventListener('deviceorientation', handleOrientationChange);
+    return () => window.removeEventListener('deviceorientation', handleOrientationChange);
   }, []);
 
+  const compassStyle = {
+    transform: `rotate(${direction}deg)`,
+  };
+
   return (
-    <div id="qibla-compass">
-      <img src="compass.png" alt="compass" />
-      {qiblaDirection !== null && (
-        <div
-          id="compass-needle"
-          style={{ transform: `rotate(${qiblaDirection}deg)` }}
-        />
-      )}
+    <div className="compass">
+      <div className="needle" style={compassStyle}></div>
     </div>
   );
-}
+};
 
 export default QiblaCompass;
+
